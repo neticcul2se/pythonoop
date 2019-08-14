eeed23d98bece87a5c119c3a0ad94ce9442b13d4
Pae <cul2se@gmail.com>
2019-08-14 23:16:36 +0700

    first

---
+++ submarine.py
@@ -0,0 +1,29 @@
+class Submarine:
+    def __init__(self):
+        self.captain = 'Prawit'
+        self.sub_name = 'Uncle I'
+        self.price = 10000
+
+    def Missile(self):
+        print("we are Department of Missile")
+
+    def Calcommission(self):
+        percent = self.price * (10/1000)
+
+
+
+kongtabreuw = Submarine()
+print(kongtabreuw.captain)
+print(kongtabreuw.sub_name)
+print('----------------')
+
+
+sub=['Srivara','Uncle II',5000]
+print(sub[0])
+print(sub[1])
+print(sub[2])
+
+
+
+
+        
