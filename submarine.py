42f8bac47b70d8b4bb3c69ef7ba5e34691eb67a6
Pae <cul2se@gmail.com>
2019-08-14 23:32:38 +0700

    fuel

--- submarine.py
+++ submarine.py
@@ -1,28 +1,39 @@
 class Submarine:
-    def __init__(self):
-        self.captain = 'Prawit'
+    def __init__(self,price):
+        self.captain = 'pravit'
         self.sub_name = 'Uncle I'
-        self.price = 10000
+        self.price = price
+        self.kilo = 0
 
     def Missile(self):
         print("we are Department of Missile")
 
     def Calcommission(self):
+        
         percent = self.price * (10/1000)
+        print('Loong you got: {} Million baht'.format(percent))
+    def Goto(self,enemypoint,distance):
+        print(f"Let's go to {enemypoint} distance :{distance} KM")
+        self.kilo= self.kilo+distance
+    def Fuel(self):
+        deisel = 20
+        
 
-
-
-kongtabreuw = Submarine()
+kongtabreuw = Submarine(20000)
 print(kongtabreuw.captain)
 print(kongtabreuw.sub_name)
 print('----------------')
+print(kongtabreuw.kilo)
+kongtabreuw.Goto('China',70000)
+print(kongtabreuw.kilo)
 
 
-sub=['Srivara','Uncle II',5000]
-print(sub[0])
-print(sub[1])
-print(sub[2])
 
+kongtabreuw.Calcommission()
+print('--------sub no.2 --------')
+kongtabbok = Submarine(30000)
+kongtabbok.captain='srevara'
+print(kongtabbok.captain)
 
 
 
